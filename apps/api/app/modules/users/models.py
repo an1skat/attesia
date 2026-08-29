cd1c2d650fbe8c075bd54de345255a1e45197ddd
W1-user <wolframgit@gmail.com>
2026-08-29 15:00:46 +0300

    chore(db): fix migrations and models

--- apps/api/app/modules/users/models.py
+++ apps/api/app/modules/users/models.py
@@ -1,21 +1,21 @@
+from typing import ClassVar
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 from .managers import UserManager
+
 
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(unique=True, db_index=True)
-    display_name = models.CharField(max_length=150, db_index=True)
-
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    if_superuser = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+    display_name = models.CharField(max_length=150)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS: ClassVar[list[str]] = []
 
     objects = UserManager()
