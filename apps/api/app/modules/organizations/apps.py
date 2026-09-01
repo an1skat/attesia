74498a15e34c0108aa3b50ac14bbd1d27eaa2d24
Antskat <156462349+an1skat@users.noreply.github.com>
2026-09-01 19:59:01 +0300

    feat(organizations): scaffold organizations module (#10)

--- apps/api/app/modules/organizations/apps.py
+++ apps/api/app/modules/organizations/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class OrganizationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"  # type: ignore
-    name = "app.modules.users"
+    name = "app.modules.organizations"
