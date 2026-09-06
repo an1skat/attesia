5de8e4c95b69d03f090d37e3a8346d6a6b38c7db
Wolfram <wolframgit@gmail.com>
2026-09-06 09:51:38 +0300

    chore(api): include registration-endpoint (#15)
    * chore(api): include registration-endpoint
    
    * chore(api): add test_missing_fields

--- apps/api/app/modules/users/api/v1/urls.py
+++ apps/api/app/modules/users/api/v1/urls.py
@@ -4,7 +4,7 @@ from .views import CustomRefreshToken, LoginView, RegisterView
 
 app_name = "users"
 urlpatterns = [
-    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/register/", RegisterView.as_view(), name="user_register"),
     path("auth/login/", LoginView.as_view(), name="token_obtain"),
     path("auth/refresh/", CustomRefreshToken.as_view(), name="token_refresh"),
 ]
