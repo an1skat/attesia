afe683ad7cc568d088d4aa120738db73aa2938fe
W1-user <wolframgit@gmail.com>
2026-09-06 18:45:56 +0300

    fix(api): users/me/

--- apps/api/app/modules/users/api/v1/urls.py
+++ apps/api/app/modules/users/api/v1/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 app_name = "users"
 urlpatterns = [
-    path("users/me", UserProfileView.as_view(), name="user_profile"),
+    path("users/me/", UserProfileView.as_view(), name="user_profile"),
     path("auth/register/", RegisterView.as_view(), name="user_register"),
     path("auth/login/", LoginView.as_view(), name="token_obtain"),
     path("auth/refresh/", CustomRefreshToken.as_view(), name="token_refresh"),
